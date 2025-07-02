7a01a967c341cdea52ed16aa18fbea51b105e22d
hmagh1 <129678389+hmagh1@users.noreply.github.com>
2025-07-02 14:15:06 +0100

    Update index.php

--- crud-php-docker/public/index.php
+++ crud-php-docker/public/index.php
@@ -9,26 +9,64 @@ $path = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 
 $controller = new UserController();
 
+// Initialiser Memcached
+$memcached = new Memcached();
+$memcached->addServer('memcached', 11211);
+
+// Point d’entrée de l’API
 if ($path[0] === 'users') {
     $id = $path[1] ?? null;
 
     switch ($method) {
         case 'GET':
-            echo $id ? $controller->getUser($id) : $controller->getAllUsers();
+            if ($id) {
+                echo $controller->getUser($id);
+            } else {
+                // Vérifier si les utilisateurs sont déjà en cache
+                $cached = $memcached->get('all_users');
+                if ($cached === false) {
+                    $response = $controller->getAllUsers();
+                    $memcached->set('all_users', $response, 300); // 5 minutes
+                    echo $response;
+                } else {
+                    echo $cached;
+                }
+            }
             break;
+
         case 'POST':
             $data = json_decode(file_get_contents('php://input'), true);
+            $memcached->delete('all_users'); // Invalider le cache
             echo $controller->createUser($data);
             break;
+
         case 'PUT':
+            if (!$id) {
+                http_response_code(400);
+                echo json_encode(["message" => "User ID is required"]);
+                break;
+            }
             $data = json_decode(file_get_contents('php://input'), true);
+            $memcached->delete('all_users'); // Invalider le cache
             echo $controller->updateUser($id, $data);
             break;
+
         case 'DELETE':
+            if (!$id) {
+                http_response_code(400);
+                echo json_encode(["message" => "User ID is required"]);
+                break;
+            }
+            $memcached->delete('all_users'); // Invalider le cache
             echo $controller->deleteUser($id);
             break;
+
         default:
             http_response_code(405);
             echo json_encode(["message" => "Method not allowed"]);
+            break;
     }
+} else {
+    http_response_code(404);
+    echo json_encode(["message" => "Endpoint not found"]);
 }
