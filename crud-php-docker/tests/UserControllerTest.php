23e59589e97d13b37fdb53898c21571c303fc393
hassan <test@gmail.com>
2025-07-02 15:11:49 +0100

    test

---
+++ crud-php-docker/tests/UserControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\UserController; // ajuste le namespace si besoin
+
+class UserControllerTest extends TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true, "PHPUnit fonctionne bien");
+    }
+}
