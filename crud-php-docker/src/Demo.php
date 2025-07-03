1f1cb6060ae1d08dd85b77797787c0d73c39e66b
hassan <test@gmail.com>
2025-07-03 10:06:22 +0100

    test de couverture

---
+++ crud-php-docker/src/Demo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class Demo {
+    public function alwaysTrue() {
+        return true;
+    }
+
+    public function alwaysFalse() {
+        return false;
+    }
+
+    public function getNumber() {
+        return 42;
+    }
+}
